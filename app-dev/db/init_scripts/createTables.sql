f20acc453e1deb3e1c4ca6808953d1f498bae4ea
mowemcfc <43922865+mowemcfc@users.noreply.github.com>
2020-05-14 17:26:15 +0800

    sql init scripts, sqlite3 db created
    Created sql create scripts according to schema image
    
    Executed scripts on sqlite3 to init DB

---
+++ app-dev/db/init_scripts/createTables.sql
@@ -0,0 +1,53 @@
+CREATE TABLE result(
+    result_id integer,
+    user_id integer,
+    test_id integer,
+    score integer,
+    marked_yesno integer,
+    PRIMARY KEY(result_id),
+    FOREIGN KEY(user_id) REFERENCES user(user_id)
+    ON DELETE SET NULL ON UPDATE NO ACTION,
+    FOREIGN KEY(test_id) REFERENCES test(test_id)
+    ON DELETE CASCADE ON UPDATE NO ACTION
+);
+
+CREATE TABLE question(
+    question_id integer,
+    question_string varchar(256),
+    answer varchar(256),
+    test_id integer,
+    mark_alloc integer,
+    PRIMARY KEY(question_id),
+    FOREIGN KEY(test_id) REFERENCES test(test_id)
+    ON DELETE SET NULL ON UPDATE NO ACTION
+);
+    
+
+CREATE TABLE user (
+    user_id integer,
+    email varchar(32),
+    password varchar(32),
+    first_name varchar(32),
+    last_name varchar(32),
+    user_course integer,
+    admin_yesno integer,
+    PRIMARY KEY(user_id)
+    FOREIGN KEY(user_course) REFERENCES course (course_id)
+    ON DELETE SET NULL ON UPDATE NO ACTION
+);
+
+CREATE TABLE course (
+    course_id integer,
+    course_name varchar(32),
+    PRIMARY KEY(course_id)
+);
+    
+CREATE TABLE test (
+    test_id integer,
+    quiz_name varchar(32),
+    course_id integer,
+    PRIMARY KEY(test_id),
+    FOREIGN KEY(course_id) REFERENCES courses(course_id)
+    ON DELETE CASCADE ON UPDATE NO ACTION
+);
+    
