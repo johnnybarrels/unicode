e1c272e7c57899c419c98354bd9260dc2e44a7b5
Johnny Barrett <barrettjc1@gmail.com>
2020-05-17 00:10:13 +0800

    jb & iv integrate flask into login and admin

--- app-dev/app/forms.py
+++ app-dev/app/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired, Email
 
 
 class LoginForm(FlaskForm):
-    email = StringField('email', validators=[DataRequired()])
+    email = EmailField('email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Login')
+    submit = SubmitField('LOGIN')
