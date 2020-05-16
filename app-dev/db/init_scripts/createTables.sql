02eaab2a2c4686048241dce993a90a2f01281fa3
mowemcfc <43922865+mowemcfc@users.noreply.github.com>
2020-05-16 12:27:31 +0800

    modify createTables.sql, add fillTables.py
    Modify sqlite3 schema for question table to include question categories. included NOT NULL's across the board.
    
    Testing of python script fillTables.py using sqlite3 module to insert rows into DB

--- app-dev/db/init_scripts/createTables.sql
+++ app-dev/db/init_scripts/createTables.sql
@@ -1,5 +1,5 @@
 CREATE TABLE result(
-    result_id integer,
+    result_id integer NOT NULL,
     user_id integer,
     test_id integer,
     score integer,
@@ -12,11 +12,12 @@ CREATE TABLE result(
 );
 
 CREATE TABLE question(
-    question_id integer,
-    question_string varchar(256),
-    answer varchar(256),
+    question_id integer NOT NULL,
+    question_string varchar(256) NOT NULL,
+    answer varchar(256) NOT NULL,
     test_id integer,
     mark_alloc integer,
+    question_group integer NOT NULL,
     PRIMARY KEY(question_id),
     FOREIGN KEY(test_id) REFERENCES test(test_id)
     ON DELETE SET NULL ON UPDATE NO ACTION
@@ -24,7 +25,7 @@ CREATE TABLE question(
     
 
 CREATE TABLE user (
-    user_id integer,
+    user_id integer NOT NULL,
     email varchar(32),
     password varchar(32),
     first_name varchar(32),
@@ -37,17 +38,18 @@ CREATE TABLE user (
 );
 
 CREATE TABLE course (
-    course_id integer,
+    course_id integer NOT NULL,
     course_name varchar(32),
     PRIMARY KEY(course_id)
 );
     
 CREATE TABLE test (
-    test_id integer,
+    test_id integer NOT NULL,
     quiz_name varchar(32),
     course_id integer,
     PRIMARY KEY(test_id),
     FOREIGN KEY(course_id) REFERENCES courses(course_id)
     ON DELETE CASCADE ON UPDATE NO ACTION
 );
-    
+   
+
