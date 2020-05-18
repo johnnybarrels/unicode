7aaac5fe14d51c498888c31fa8d7ae4c6d54c6e4
Johnny Barrett <barrettjc1@gmail.com>
2020-05-18 13:47:39 +0800

    change test users and courses to generic names

--- app-dev/app/db_playtime.py
+++ app-dev/app/db_playtime.py
@@ -1,19 +1,19 @@
 from app import db
 from app.models import User, Course
 
-jb = User(id=25, email='johnny@barrels.com', first_name='Johnny',
+u1 = User(id=25, email='student@test.com', first_name='Student', last_name='Test',
           password_hash='pbkdf2:sha256:150000$nfbYPHOa$b054a2c58abc670645958066b3d1ff702ffcb66dafb1c632566a5b4105d71480',
-          last_name='Barrett', is_admin=0)
+          is_admin=0)
 
-tim = User(id=10, email='tim@french.com', first_name='Tim',
+u2 = User(id=10, email='admin@test.com', first_name='Admin', last_name='Test', 
           password_hash='pbkdf2:sha256:150000$nfbYPHOa$b054a2c58abc670645958066b3d1ff702ffcb66dafb1c632566a5b4105d71480',
-          last_name='French', is_admin=1)
+          is_admin=1)
 
 c5504 = Course(id=5504, course_name='Agile Web Development')
 
 c1401 = Course(id=1401, course_name='Intro to Python')
 
-jb.courses.append(c1401)
-tim.courses.append(c5504)
+u1.courses.append(c1401)
+u2.courses.append(c5504)
 
-db.session.add_all([jb, tim])
+db.session.add_all([u1, u2])
