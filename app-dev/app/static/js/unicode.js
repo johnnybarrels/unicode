2293d1c99b19bdebb4e3d538646f8b63558be501
Johnny Barrett <barrettjc1@gmail.com>
2020-05-19 23:59:06 +0800

    merge with ivy frontend

--- app-dev/app/static/js/unicode.js
+++ app-dev/app/static/js/unicode.js
@@ -1,37 +1,42 @@
-$(window).ready( function () {
+$(window).ready(function () {
     let sideNavWidth = $('.sidenav').css('width');
     $('.content-container').css('margin-left', sideNavWidth);
 
-    $(window).on('resize', function() {
+    $(window).on('resize', function () {
         let sideNavWidth = $('.sidenav').css('width');
         $('.content-container').css('margin-left', sideNavWidth);
+    });
+
+    $(".fa-caret-square-down").click(function () {
+        $("#option-menu").toggle();
+    });
+    // $(".fa-times").click(function () {
+    //     document.getElementById("option-menu").style.visibility = "hidden";
+    // });
+
+    $(".fa-plus-circle").click(function () {
+        $("#new-course-modal").modal("show");
+    });
+    $("#new-test-btn").click(function () {
+        $("#new-test-modal").modal("show");
     });
 }
 );
 
 
-$("document").ready(function(){
-    $(".fa-caret-square-down").click(function(){
-            document.getElementById("option-menu").style.visibility="visible";
-    });
-  });
+// $(document).ready(function () {
 
-$("document").ready(function(){
-    $(".fa-times").click(function(){ 
-        document.getElementById("option-menu").style.visibility="hidden";
-    });
-});
+// });
+
+// $("document").ready(function () {
+
+// });
 
 
+// $("document").ready(function () {
 
-$("document").ready(function(){
-    $(".fa-plus-circle").click(function(){ 
-        $("#new-course-modal").modal("show");
-    });
-});
+// });
 
-$("document").ready(function(){
-    $("#new-test-btn").click(function(){ 
-        $("#new-test-modal").modal("show");
-    });
-});
+// $("document").ready(function () {
+
+// });
