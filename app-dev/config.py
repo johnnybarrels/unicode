89a06c40f001f38eaa5ef6afeac31e05d5f9e4cc
Johnny Barrett <barrettjc1@gmail.com>
2020-11-12 11:02:57 +0800

    add runtime.txt

--- app-dev/config.py
+++ app-dev/config.py
@@ -8,10 +8,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config():
 
     # Creation of secret key for WTForms
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-secret-agile-key'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'db/unicode.db')
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    #     'sqlite:///' + os.path.join(basedir, 'db/unicode.db')
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
     SQLALCHEMY_ECHO = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
