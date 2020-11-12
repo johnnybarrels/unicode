89b0a509afe9c5e8ff4f47904827e6cf71d7df9d
Johnny Barrett <barrettjc1@gmail.com>
2020-11-12 11:49:34 +0800

    change config.py

--- app-dev/config.py
+++ app-dev/config.py
@@ -10,10 +10,10 @@ class Config():
     # Creation of secret key for WTForms
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-    #     'sqlite:///' + os.path.join(basedir, 'db/unicode.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'db/unicode.db')
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
     SQLALCHEMY_ECHO = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
